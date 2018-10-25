931a140aef84bc809391fe41587a98f47b508472
YeonhuiKim <34934159+YeonhuiKim@users.noreply.github.com>
2018-10-25 19:30:06 +0900

    Add files via upload

---
+++ source/mainmenu_quit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu_quit : MonoBehaviour {
+    public void Mainmenu_quit()
+    {
+        Application.Quit();
+    }
+}
